fca3695633ff5f52c80a8b8d298d2a1f93c3877c
krzysdz <krzysdz@users.noreply.github.com>
2024-04-27 00:48:35 +0200

    [lab2] Code formatting

--- lab2/RegulatorPID.cpp
+++ lab2/RegulatorPID.cpp
@@ -3,9 +3,10 @@
 #include <cmath>
 #include <stdexcept>
 
-constexpr void RegulatorPID::check_constraints() const {
-	if (is_bad_or_neg(m_k) || is_bad_or_neg(m_ti) || is_bad_or_neg(m_td))
-		throw std::runtime_error{"PID parameters must be nonnegative finite numbers"};
+constexpr void RegulatorPID::check_constraints() const
+{
+    if (is_bad_or_neg(m_k) || is_bad_or_neg(m_ti) || is_bad_or_neg(m_td))
+        throw std::runtime_error{ "PID parameters must be nonnegative finite numbers" };
 }
 
 constexpr double RegulatorPID::symuluj(double u) { return 0.0; }
