63b17fa88f56b424ab225e79ceaa6c3f2ab1f8eb
krzysdz <krzysdz@users.noreply.github.com>
2024-04-29 15:53:56 +0200

    [lab2] Add feedback loop simulation
    TODO: Check correctness, ASAN gets stuck in an infinite loop on
    Ubuntu 23.10, maybe it's just Ubuntu, maybe I did something wrong.

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -219,11 +219,69 @@ int main()
 #endif
 
 #ifdef MAIN
+#include "../lab1/ModelARX.h"
+#include <optional>
+
+double feedback_step(RegulatorPID &regulator, ModelARX &model, const double u,
+                     const std::optional<double> &reset = std::nullopt)
+{
+    static double last_output = 0.0;
+    if (reset.has_value())
+        last_output = reset.value();
+    const auto e = u - last_output;
+    const auto input = regulator.symuluj(e);
+    last_output = model.symuluj(input);
+    return last_output;
+}
+
+ModelARX get_model() { return ModelARX{ { -0.4 }, { 0.6 }, 1, 0.0 }; }
+
+void run_sim(RegulatorPID &pid)
+{
+    constexpr std::size_t steps{ 30 };
+    auto m = get_model();
+    std::cout << feedback_step(pid, m, 0.0, 0.0);
+    for (auto i = 1UL; i < steps; ++i)
+        std::cout << ' ' << feedback_step(pid, m, 1.0);
+    std::cout << std::endl;
+}
+
+void simulate_p_1()
+{
+    std::cout << "P regulator [k = 0.5]\n";
+    RegulatorPID p{ 0.5 };
+    run_sim(p);
+}
+
+void simulate_p_2()
+{
+    std::cout << "P regulator [k = 2.0]\n";
+    RegulatorPID p{ 2.0 };
+    run_sim(p);
+}
+
+void simulate_pi_1()
+{
+    std::cout << "PI regulator [k = 0.5, Ti = 10.0]\n";
+    RegulatorPID pi{ 0.5, 10.0 };
+    run_sim(pi);
+}
+
+void simulate_pi_2()
+{
+    std::cout << "PI regulator [k = 0.4, Ti = 2.0]\n";
+    RegulatorPID pi{ 0.4, 2.0 };
+    run_sim(pi);
+}
 
 using namespace std;
 
 int main()
 {
     Testy_RegulatorPID::run_tests();
+    simulate_p_1();
+    simulate_p_2();
+    simulate_pi_1();
+    simulate_pi_2();
 }
 #endif
