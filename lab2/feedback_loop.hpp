55c0dda16652ec284ba4398a0278bb187e9571f8
krzysdz <krzysdz@users.noreply.github.com>
2024-05-03 02:13:26 +0200

    [lab2] Small improvements
    - Use correct case in `#include`
    - Disable GNU extensions in CMake for better portability

--- lab2/feedback_loop.hpp
+++ lab2/feedback_loop.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <optional>
 #include "../lab1/ModelARX.h"
-#include "RegulatorPid.h"
+#include "RegulatorPID.h"
 
 double feedback_step(RegulatorPID &regulator, ModelARX &model, const double u,
                      const std::optional<double> &reset = std::nullopt);
